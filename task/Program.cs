b346e8fa0ae8aeee8e710dacf80eca223963c616
stinqyy <maxvik19m@mail.ru>
2023-02-24 12:02:49 +0300

    Создал функцию поиска строк <= 3 из массива

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] array = new string[4] {"hello", "2", "world", ":-)"};
 string[] result = new string[array.Length];
+
+void ArraySearch(string[] array, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        result[count] = array[i];
+        count++;
+        }
+    }
+}
